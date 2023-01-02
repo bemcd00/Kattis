071fd4aaf1b9e64bf7ca58fb53ac835afd531294
Briannah McDaniel <109481233+bemcd00@users.noreply.github.com>
2023-01-02 12:14:11 -0600

    Create triar.c

---
+++ triar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+        int b;
+        int h;
+        float area;
+
+        scanf("%d %d", &b, &h);
+
+        area = 0.5*b*h;
+
+        printf("%.7f", area);
+
+
+
+        return 0;
+}
