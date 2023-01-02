2857083e99f1f4b31c7c21eaa2a93e381305b2df
Briannah McDaniel <109481233+bemcd00@users.noreply.github.com>
2023-01-02 13:44:34 -0600

    Create R2.c

---
+++ R2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+
+        int r1;
+        int r2;
+        int s;
+
+        scanf("%d %d", &r1, &s);
+
+        r2 = 2*s-r1;
+        printf("%d", r2);
+
+        return 0;
+}
