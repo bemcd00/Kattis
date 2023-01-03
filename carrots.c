3b5ad01797ebd1ee17cd688f5d7d47f8c9f29e02
Briannah McDaniel <109481233+bemcd00@users.noreply.github.com>
2023-01-03 13:32:08 -0600

    Create carrots.c

---
+++ carrots.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main() {
+
+        int n;
+        int p;
+        char arr[100];
+        int i;
+
+
+        scanf("%d %d", &n, &p);
+
+        for (i=0; i < n; i++) {
+
+                scanf("%s", arr);
+
+        }
+
+        printf("%d", p);
+
+
+        return 0;
+}
